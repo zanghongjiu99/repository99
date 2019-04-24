6b6c0843b856c1d419e40110bee0d5afb1a64ca0
zhj <zanghongjiu@sina.com>
2019-04-24 17:59:14 +0800

    xx

---
+++ src/main/java/com/zhj/service/OrderService.java
@@ -0,0 +1,5 @@
+package com.zhj.service;
+
+public class OrderService {
+
+}
---
+++ src/main/java/com/zhj/service/UserServiceImple.java
@@ -0,0 +1,7 @@
+package com.zhj.service;
+
+public class UserServiceImple {
+    public void updateUser(){
+        System.out.println("updateUser in server");
+    }
+}
